d31ef6f7caa98d918d20e4b1b6b1df9cbf288743
Antoine NEUVILLE <antoine.neuville@student.junia.com>
2023-11-03 19:59:26 +0100

    empty commit

--- music-core/src/main/java/florent/jee/webapp/core/entity/Album.java
+++ music-core/src/main/java/florent/jee/webapp/core/entity/Album.java
@@ -14,8 +14,6 @@ public class Album extends GenericEntity implements Comparable<Album> {
     @OneToMany(mappedBy = "album")
     private List<Artist> artists;
 
-    @OneToMany
-
     public Album() {
     }
 
--- music-core/src/main/java/florent/jee/webapp/core/entity/GenericEntity.java
+++ music-core/src/main/java/florent/jee/webapp/core/entity/GenericEntity.java
@@ -16,3 +16,4 @@ public abstract class GenericEntity {
         return id;
     }
 }
+
